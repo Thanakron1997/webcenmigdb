825fd1ecb8d3cffec80dabec4197f65a44e39552
Thanakron1997 <thanakron.nop@gmail.com>
2024-07-18 17:39:33 +0700

    update api

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask,render_template,make_response,jsonify, request,send_file
 from flask_wtf import FlaskForm
-from wtforms import TextField,SubmitField,RadioField
+from wtforms import TextField,SubmitField
 import pandas as pd
 import ast
 from function.web_cenmig import count_seq,dashboard_data,count_st_lineage
 from function.api import get_item_from_db,generate_zip
-from function.connect import connect_mongodb
+from function.connect import connect_mongodb,getApiKey
 import json
 from io import BytesIO
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 client = connect_mongodb()
 db = client['metadata']
 mycol = db["bacteria"]
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'  
+API_KEY = getApiKey()
+
+# Initialize Limiter
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["30 per minute"]  # Set global rate limits
+    ,storage_uri="memory://"
+)
 
 class MyForm(FlaskForm):
     name = TextField("CenmigID :")
@@ -23,10 +34,6 @@ class Formfixible(FlaskForm):
     fixiblequery = TextField("Put Your Query Here")
     submit2 = SubmitField("Download Data")
 
-class DownloadSequences(FlaskForm):
-    theQuery = TextField("Put Your Query Here") 
-    select_file = RadioField("Select Your file type", choices=["Raw-Sequences-Only","Assembly-Only", "All-Type"])
-    submit3 = SubmitField("Download Sequences")
 class query_dashboard_form(FlaskForm):
     query_dash = TextField("Put Your Query Here")
     query_dash_submit = SubmitField("Submit")
@@ -94,7 +101,6 @@ def download_data():
                 query_data = mycol.find(query1, {'_id': 0})
                 df = pd.DataFrame.from_dict(query_data)
                 csv = df.to_csv(index=False)
-                # name_file = 'File_Result_{}.csv'.format(pd.datetime.now().strftime("%Y-%m-%d %H%M%S"))
                 query_res = 'download'      
             except:
                 query_res = 'No Data Found'
@@ -156,175 +162,212 @@ def bac_strep():
 @app.route('/api/metadata/search', methods=['GET'])
 def get_data():
     query_params = dict(request.args)
-    list_query = list(query_params.keys())
-    print(query_params)
-    if len(list_query) <= 1:
-        pipeline = [
-            { '$match': { list_query[0]: query_params[list_query[0]] } },
-            {
-                '$lookup': {
-                    'from': 'mlst',  
-                    'localField': 'cenmigID', 
-                    'foreignField': 'cenmigID',  
-                    'as': 'mlst'  
-                }
-            },
-            {
-                '$unwind': {'path': '$mlst', 'preserveNullAndEmptyArrays': True} 
-            },
-            {
-                '$project': {
-                    'mlst._id': 0,
-                    '_id': 0,
-                    'mlst.cenmigID': 0 
-                }
-            },
-            {
-                '$lookup': {
-                    'from': 'drug_resistance_resfinder',  
-                    'localField': 'cenmigID',  
-                    'foreignField': 'cenmigID', 
-                    'as': 'drug_resistance_resfinder'  
-                }
-            },
-            {
-                '$unwind': {'path': '$drug_resistance_resfinder', 'preserveNullAndEmptyArrays': True} 
-            },
-            {
-                '$project': {
-                    'drug_resistance_resfinder._id': 0,
-                    'drug_resistance_resfinder.cenmigID': 0 
-                }
-            },
-            {
-                '$lookup': {
-                    'from': 'point_mutation_pointfinder',  
-                    'localField': 'cenmigID', 
-                    'foreignField': 'cenmigID',
-                    'as': 'point_mutation_pointfinder'  
-                }
-            },
-            {
-                '$unwind': {'path': '$point_mutation_pointfinder', 'preserveNullAndEmptyArrays': True} 
-            },
-            {
-                '$project': {
-                    'point_mutation_pointfinder._id': 0,
-                    'point_mutation_pointfinder.cenmigID': 0 
-                }
-            },
-            {
-                '$lookup': {
-                    'from': 'tb_profiler',  
-                    'localField': 'cenmigID', 
-                    'foreignField': 'cenmigID',  
-                    'as': 'tb_profiler'  
-                }
-            },
-            {
-                '$unwind': {'path': '$tb_profiler', 'preserveNullAndEmptyArrays': True} 
-            },
-            {
-                '$project': {
-                    'tb_profiler._id': 0,
-                    'tb_profiler.cenmigID': 0,
-                    # 'tb_profiler.TB_raw_result' :0
-                }
-            }
-            
-        ]
-        result = db.bacteria.aggregate(pipeline)
-        res = {'data': list(result)} 
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
     else:
-        return jsonify({"Error!": "Now cant use more than one query"}), 400
-    if not query_params:
-        return jsonify({"Error!": "No query provided for retrieving data"}), 400
-    return jsonify(res)
+        list_query = list(query_params.keys())
+        if len(list_query) <= 2:
+            pipeline = [
+                { '$match': { 'cenmigID': query_params['cenmigID'] } },
+                {
+                    '$lookup': {
+                        'from': 'mlst',  
+                        'localField': 'cenmigID', 
+                        'foreignField': 'cenmigID',  
+                        'as': 'mlst'  
+                    }
+                },
+                {
+                    '$unwind': {'path': '$mlst', 'preserveNullAndEmptyArrays': True} 
+                },
+                {
+                    '$project': {
+                        'mlst._id': 0,
+                        '_id': 0,
+                        'mlst.cenmigID': 0 
+                    }
+                },
+                {
+                    '$lookup': {
+                        'from': 'drug_resistance_resfinder',  
+                        'localField': 'cenmigID',  
+                        'foreignField': 'cenmigID', 
+                        'as': 'drug_resistance_resfinder'  
+                    }
+                },
+                {
+                    '$unwind': {'path': '$drug_resistance_resfinder', 'preserveNullAndEmptyArrays': True} 
+                },
+                {
+                    '$project': {
+                        'drug_resistance_resfinder._id': 0,
+                        'drug_resistance_resfinder.cenmigID': 0 
+                    }
+                },
+                {
+                    '$lookup': {
+                        'from': 'point_mutation_pointfinder',  
+                        'localField': 'cenmigID', 
+                        'foreignField': 'cenmigID',
+                        'as': 'point_mutation_pointfinder'  
+                    }
+                },
+                {
+                    '$unwind': {'path': '$point_mutation_pointfinder', 'preserveNullAndEmptyArrays': True} 
+                },
+                {
+                    '$project': {
+                        'point_mutation_pointfinder._id': 0,
+                        'point_mutation_pointfinder.cenmigID': 0 
+                    }
+                },
+                {
+                    '$lookup': {
+                        'from': 'tb_profiler',  
+                        'localField': 'cenmigID', 
+                        'foreignField': 'cenmigID',  
+                        'as': 'tb_profiler'  
+                    }
+                },
+                {
+                    '$unwind': {'path': '$tb_profiler', 'preserveNullAndEmptyArrays': True} 
+                },
+                {
+                    '$project': {
+                        'tb_profiler._id': 0,
+                        'tb_profiler.cenmigID': 0,
+                        # 'tb_profiler.TB_raw_result' :0
+                    }
+                }
+                
+            ]
+            result = db.bacteria.aggregate(pipeline)
+            res = {'data': list(result)} 
+        else:
+            return jsonify({"Error!": "Not have cenmigID in database"}), 400
+        if not query_params:
+            return jsonify({"Error!": "No query provided for retrieving data"}), 400
+        return jsonify(res)
 
 @app.route('/api/download-seq', methods=['GET'])
 def download_seq():
-    query = request.args
-    if "cenmigID" in query:
-        id_ = query['cenmigID']
-        myquery = {'cenmigID': id_}
-        result_data = mycol.find_one(myquery, {'_id': 0,'cenmigID':1,'file_name':1,})
-        if result_data:
-            if "file_name" in result_data:
-                file_list = result_data['file_name'].split(", ")
-                if len(file_list)==1:
-                    file_data = get_item_from_db(client,file_list[0])
-                    return send_file(BytesIO(file_data), download_name=file_list[0], as_attachment=True)
-                else:
-                    files = []
-                    for i in file_list:
-                        file_data = get_item_from_db(client,i)
-                        files.append((i, file_data))
-                    full_zip_in_memory = generate_zip(files)
-                    return send_file(BytesIO(full_zip_in_memory), download_name="seq_data.zip", as_attachment=True)
-            else:
-                return jsonify({"Error!": "No sequence file!"}), 400
-        else:
-            return jsonify({"Error!": "No cenmigID in database!"}), 400
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
     else:
-        return jsonify({"Error!": "No cenmigID key"}), 400
+        query = request.args
+        if "cenmigID" in query:
+            id_ = query['cenmigID']
+            myquery = {'cenmigID': id_}
+            result_data = mycol.find_one(myquery, {'_id': 0,'cenmigID':1,'file_name':1,})
+            if result_data:
+                if "file_name" in result_data:
+                    file_list = result_data['file_name'].split(", ")
+                    if len(file_list)==1:
+                        file_data = get_item_from_db(client,file_list[0])
+                        return send_file(BytesIO(file_data), download_name=file_list[0], as_attachment=True)
+                    else:
+                        files = []
+                        for i in file_list:
+                            file_data = get_item_from_db(client,i)
+                            files.append((i, file_data))
+                        full_zip_in_memory = generate_zip(files)
+                        return send_file(BytesIO(full_zip_in_memory), download_name="seq_data.zip", as_attachment=True)
+                else:
+                    return jsonify({"Error!": "No sequence file!"}), 400
+            else:
+                return jsonify({"Error!": "No cenmigID in database!"}), 400
+        else:
+            return jsonify({"Error!": "No cenmigID key"}), 400
 
 @app.route('/api/metadata/mycobacterium_tuberculosis', methods=['GET'])
 def get_data_mtb():
-    myquery = {'Organism' : { "$regex": "^Mycobacterium" }}
-    query_params = dict(request.args)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'DR_Type':1,'Collection_date':1,'Platform':1,'wg_snp_lineage_assignment':1,'Assay_Type':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Mycobacterium" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'DR_Type':1,'Collection_date':1,'Platform':1,'wg_snp_lineage_assignment':1,'Assay_Type':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/salmonella_enterica', methods=['GET'])
 def get_data_salmo():
-    myquery = {'Organism' : { "$regex": "^Salmonella" }}
-    query_params = dict(request.args)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Salmonella" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/staphylococcus', methods=['GET'])
 def get_data_staphylococcus():
-    myquery = {'Organism' : { "$regex": "^Staphylococcus" }}
-    query_params = dict(request.args)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Staphylococcus" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/streptococcus_agalactiae', methods=['GET'])
 def get_data_streptococcus_agalactiae():
-    myquery = {'Organism' : { "$regex": "^Streptococcus agalactiae" }}
-    query_params = dict(request.args)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Streptococcus agalactiae" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/campylobacter_jejuni', methods=['GET'])
 def get_data_campylobacter_jejuni():
-    myquery = {'Organism' : { "$regex": "^Campylobacter jejuni" }}
-    query_params = dict(request.args)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Campylobacter jejuni" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/candida_glabrata', methods=['GET'])
 def get_data_candida_glabrata():
-    myquery = {'Organism' : { "$regex": "^Candida glabrata" }}
-    query_params = dict(request.args)
-    print(query_params)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Candida glabrata" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
 
 @app.route('/api/metadata/burkholderia_pseudomallei', methods=['GET'])
 def get_data_burkholderia_pseudomallei():
-    myquery = {'Organism' : { "$regex": "^Burkholderia" }}
-    query_params = dict(request.args)
-    print(query_params)
-    myquery.update(query_params)
-    result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
-    return jsonify(result_data)
+    api_key = request.args.get('api_key')
+    if api_key != API_KEY:
+        return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        myquery = {'Organism' : { "$regex": "^Burkholderia" }}
+        query_params = dict(request.args)
+        myquery.update(query_params)
+        result_data = list(mycol.find(myquery, {'_id': 0,'cenmigID':1,'Organism':1,'geo_loc_name_country_fix':1,'Assay_Type':1,'Collection_date':1,'Platform':1,'ST':1})) 
+        return jsonify(result_data)
+
+@app.route('/api_doc')
+def api_doc():
+    return render_template("api_doc.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port='8080')
--- function/connect.py
+++ function/connect.py
@@ -1,6 +1,7 @@
 
 import pymongo
 import os
+import hashlib 
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,3 +12,11 @@ def connect_mongodb():
         password=os.getenv('MONGODB_PWD'),
     )
     return client
+
+def getApiKey():
+    client = connect_mongodb()
+    db = client['api']
+    mycol = db["api_key"]
+    dquery = mycol.find_one({}, {'_id': 0})
+    result = hashlib.sha256(dquery['api_key'].encode())
+    return result.hexdigest()
